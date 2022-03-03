02184f12651e4f326ef344ee340be51d17bc8fff
Fuzss <28218241+Fuzss@users.noreply.github.com>
2022-03-03 20:43:25 +0100

    compiled for 1.18.2

--- src/main/java/fuzs/deathcompass/DeathCompass.java
+++ src/main/java/fuzs/deathcompass/DeathCompass.java
@@ -10,15 +10,15 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLConstructModEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Mod(DeathCompass.MOD_ID)
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class DeathCompass {
     public static final String MOD_ID = "deathcompass";
     public static final String MOD_NAME = "Death Compass";
-    public static final Logger LOGGER = LogManager.getLogger(DeathCompass.MOD_NAME);
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_NAME);
 
     @SuppressWarnings("Convert2MethodRef")
     public static final ConfigHolder<AbstractConfig, ServerConfig> CONFIG = ConfigHolder.server(() -> new ServerConfig());
